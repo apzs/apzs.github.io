27b9a2b228b9b3ff323659b6790b0a7ac0dcad28
apzs <72078453+apzs@users.noreply.github.com>
2023-12-30 13:53:29 +0000

    Deploying to gh-pages from @ apzs/apzs.github.io@80579c0de2793b2623dd9b20cec7b686174088ab 🚀

---
+++ assets/pageview-38FMMqgY.js
@@ -0,0 +1,7 @@
+import{a9 as t}from"./app-DmUHAZrg.js";var a={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://comment-rose-alpha.vercel.app"};const n=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-DmUHAZrg.js").then(r=>r.ah),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{n as updatePageview};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
